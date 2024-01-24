c38d860a37c9d143c5c72b3294534b4d06e53815
kiralin <highfoot908@livemail.tw>
2024-01-24 17:33:26 +0800

    Initial Commit

---
+++ Japanese50/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Japanese50
+//
+//  Created by 林郁琦 on 2024/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
